21745c46ba07c09c964231ffd395f43797b41664
Georgi Georgiev <georgiev.georgi@siemens.com>
2024-11-24 11:17:41 +0200

    EC2 instance and S3 bucket

--- lib/aws-exam-2410-gg-stack.ts
+++ lib/aws-exam-2410-gg-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam'
 
 export class AwsExam2410GgStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+     // S3 Bucket
+     const bucket = new s3.Bucket(this, 'Exam-Bucket-GG', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      lifecycleRules: [{ expiration: cdk.Duration.minutes(30) }]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsExam2410GgQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // EC2 Instance
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
+    const instance = new ec2.Instance(this, 'MyInstance', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2(), 
+      vpc
+    });
+
+    // Add IAM Role to EC2 Instance
+    instance.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
+    instance.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AWSLambdaExecute'));
+
+    // Output the S3 bucket name
+    new cdk.CfnOutput(this, 'BucketName', { value: bucket.bucketName });
   }
 }
